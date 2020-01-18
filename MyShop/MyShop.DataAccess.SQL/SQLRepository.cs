bbadfad5fff00090f3071c311ebe44fa72fed9f7
Akash Agrawal <cs.akash.10@gmail.com>
2020-01-19 00:16:52 +0530

    connect with SQL database

---
+++ MyShop/MyShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,60 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+	public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+	{
+		internal DataContext dataContext;
+		internal DbSet<T> dbSet;
+
+		public SQLRepository(DataContext dataContext)
+		{
+			this.dataContext = dataContext;
+			this.dbSet = dataContext.Set<T>();
+		}
+		public IQueryable<T> Collection()
+		{
+			return dbSet;
+		}
+
+		public void Commit()
+		{
+			dataContext.SaveChanges();
+		}
+
+		public void Delete(string Id)
+		{
+			var t = Find(Id);
+
+			if(dataContext.Entry(t).State == EntityState.Detached)
+			{
+				dbSet.Attach(t);				
+			}
+
+			dbSet.Remove(t);
+		}
+
+		public T Find(string Id)
+		{
+			return dbSet.Find(Id);
+		}
+
+		public void Insert(T p)
+		{
+			dbSet.Add(p);
+		}
+
+		public void Update(T t)
+		{
+			dbSet.Attach(t);
+			dataContext.Entry(t).State = EntityState.Modified;
+		}
+	}
+}
