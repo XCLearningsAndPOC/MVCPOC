978dcf41e600c1574a6d815678edec481519e425
Akash Agrawal <cs.akash.10@gmail.com>
2020-01-19 14:39:08 +0530

    adding filters

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,25 +11,40 @@ namespace MyShop.WebUI.Controllers
 {
 	public class HomeController : Controller
 	{
-		IRepository<Product> context;
-		IRepository<ProductCategory> productCategories;
+		IRepository<Product> productContext;
+		IRepository<ProductCategory> productCategoryContext;
 
 		public HomeController(IRepository<Product> productRepository,
 			IRepository<ProductCategory> productCategoryRepository)
 		{
-			context = productRepository;
-			productCategories = productCategoryRepository;
+			productContext = productRepository;
+			productCategoryContext = productCategoryRepository;
 		}
 
-		public ActionResult Index()
+		public ActionResult Index(string Category = null)
 		{
-			List<Product> products = context.Collection().ToList();
-			return View(products);
+			List<Product> products = productContext.Collection().ToList();
+			List<ProductCategory> productCategories = productCategoryContext.Collection().ToList();
+
+			if (Category == null)
+			{
+				products = productContext.Collection().ToList();
+			}
+			else
+			{
+				products = productContext.Collection().Where(p => p.Category == Category).ToList();
+			}
+
+			ProductListViewModel productListViewModel = new ProductListViewModel();
+			productListViewModel.Products = products;
+			productListViewModel.ProductCategories = productCategories;
+
+			return View(productListViewModel);
 		}
 
 		public ActionResult Details(string Id)
 		{
-			Product product = context.Find(Id);
+			Product product = productContext.Find(Id);
 			if (product == null)
 			{
 				return HttpNotFound();
